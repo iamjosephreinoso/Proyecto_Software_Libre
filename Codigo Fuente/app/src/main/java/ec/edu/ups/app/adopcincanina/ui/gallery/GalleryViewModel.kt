572d372a315c10e304e4bf20cc6c4dc9be401605
iamjosephreinoso <joreinoso39@gmail.com>
2020-11-24 10:01:33 -0500

    Proyecto Software Libre

---
+++ Codigo Fuente/app/src/main/java/ec/edu/ups/app/adopcincanina/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package ec.edu.ups.app.adopcincanina.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
